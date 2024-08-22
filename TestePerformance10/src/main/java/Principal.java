5fe40de80c1899b633a68debc08d0b15b34bde4b
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2024-08-22 15:29:10 -0300

    Update Principal.java

--- TestePerformance10/src/main/java/Principal.java
+++ TestePerformance10/src/main/java/Principal.java
@@ -2,7 +2,7 @@ public class Principal {
     public static void main(String[] args) {
         Aluno aluno = new Aluno();
        aluno.setMatricula(1);
-       aluno.setNome("Rodrigo");
+       aluno.setNome("Rodrigo Junior");
         System.out.println(aluno.getMatricula() + aluno.getNome());
     }
 }
